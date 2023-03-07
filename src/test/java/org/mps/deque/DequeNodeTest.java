73a6e26d3156f7d5cbe3d84cddeed6f17f838341
ignamarga1 <0619967319@uma.es>
2023-03-07 11:38:08 +0100

    Updated pom.xml

--- src/test/java/org/mps/deque/DequeNodeTest.java
+++ src/test/java/org/mps/deque/DequeNodeTest.java
@@ -1,4 +1,13 @@
 package org.mps.deque;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Class for testing DequeNode.java
+ *
+ * @author Ignacio Martínez Gallardo
+ */
+
 class DequeNodeTest {
+
 }
