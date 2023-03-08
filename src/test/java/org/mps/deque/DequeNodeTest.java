ebe15ba6656b3fd96e472df4d8b8fdd66a763c88
ignamarga1 <0619967319@uma.es>
2023-03-08 18:05:20 +0100

    Primeros casos de prueba añadidos

--- src/test/java/org/mps/deque/DequeNodeTest.java
+++ src/test/java/org/mps/deque/DequeNodeTest.java
@@ -14,26 +14,54 @@ import static org.junit.jupiter.api.Assertions.*;
 class DequeNodeTest {
     DequeNode<Integer> node;
 
-    @BeforeEach
-    void setup() {
-        node = new DequeNode<>(1, null, null);
-    }
+    class TestCasesForOneElementDeque {
+        @BeforeEach
+        void setup() {
+            node = new DequeNode<>(1, null, null);
+        }
 
-    @AfterEach
-    void shutdown() {
-        node = null;
-    }
+        @AfterEach
+        void shutdown() {
+            node = null;
+        }
 
-    @Test
-    void shouldReturnTheNodeItem() {
-        int expectedValue = 1;
-        int actualValue = node.getItem();
+        @Test
+        void shouldReturnTheNodeItem() {
+            int expectedValue = 1;
+            int actualValue = node.getItem();
 
-        assertEquals(expectedValue, actualValue);
-    }
+            assertEquals(expectedValue, actualValue);
+        }
 
-    @Test
-    void theComputedMethodShouldReturnTheSetItem() {
+        @Test
+        void shouldSetTheNodeItemTo2() {
+            node.setItem(2);
+            assertEquals(2, node.getItem());
+        }
 
+        @Test
+        void shouldReturnThePreviousNode() {
+            assertNull(node.getPrevious());
+        }
+
+        @Test
+        void shouldReturnTheNextNode() {
+            assertNull(node.getNext());
+        }
+
+        @Test
+        void shouldReturnTrueToNodeBeingTheFirstNodeOfTheDequeue() {
+            assertTrue(node.isFirstNode());
+        }
+
+        @Test
+        void shouldReturnTrueToNodeBeingTheLastNodeOfTheDequeue() {
+            assertTrue(node.isLastNode());
+        }
+
+        @Test
+        void shouldReturnFalseToNodeNotBeingATerminalNode() {
+            assertFalse(node.isNotATerminalNode());
+        }
     }
 }
