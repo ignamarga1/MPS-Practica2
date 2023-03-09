1924fbebc3753f4c515cf3fd13786ef7456fb445
PablosGonUMA <pablosgon@uma.es>
2023-03-09 17:03:51 +0100

    Agregados Nested a la clase DoublyLinkedListDequeTest

--- src/test/java/org/mps/deque/DoublyLinkedListDequeTest.java
+++ src/test/java/org/mps/deque/DoublyLinkedListDequeTest.java
@@ -1,10 +1,6 @@
 package org.mps.deque;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Nested;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,155 +17,178 @@ class DoublyLinkedListDequeTest {
     DoubleEndedQueue<Integer> deque;
 
     @Nested
-    @DisplayName("Clase 1")
-    class Clase1{
+    @DisplayName("Casos de prueba de la creación de deques")
+    class NewDeque{
+        @BeforeEach
+        void createDeque(){
+            deque = new DoublyLinkedListDeque<>();
+        }
         @Test
         void newDequeFirstIsNull(){
             assertNull(deque.first());
         }
+
+        @Test
+        void newDequeLastIsNull(){
+            assertNull(deque.last());
+        }
+
+        @Test
+        void newDequeSizeIs0(){
+            assertEquals(0, deque.size());
+        }
+    }
+
+    @Nested
+    @DisplayName("Casos de prueba de añadir un nodo al principio")
+    class PrependingNewNodes{
+        @Test
+        void prependingNodeInEmptyDequeIsTheFirstNode(){
+            int item = 1;
+            deque.prepend(item);
+            assertEquals(item, deque.first());
+        }
+
+        @Test
+        void prependingNodeInEmptyDequeIsAlsoTheLastNode(){
+            int item = 1;
+            deque.prepend(item);
+            assertEquals(item, deque.last());
+        }
+
+        @Test
+        void prependingANodeInEmptyDequeMakesItHaveSize1(){
+            int item = 1;
+            deque.prepend(item);
+            assertEquals(deque.size(), 1);
+        }
+
+        @Test
+        void prependingNodeInDequeIsFirst() {
+            int item1 = 1;
+            int item2 = 2;
+            deque.prepend(item1);
+            deque.prepend(item2);
+            assertEquals(item2, deque.first());
+        }
+
+        @Test
+        void prependingANodeIncreasesSizeBy1(){
+            int item1 = 1;
+            int item2 = 2;
+            deque.prepend(item1);
+
+            int prevSize = deque.size();
+
+            deque.prepend(item2);
+            assertEquals(prevSize + 1, deque.size());
+        }
+    }
+
+    @Nested
+    @DisplayName("Casos de prueba de añadir un nodo al final")
+    class appendingNewNodes{
+        @Test
+        void appendingNodeInEmptyDequeIsLastNode(){
+            int item = 1;
+            deque.append(1);
+            assertEquals (item, deque.last());
+        }
+
+        @Test
+        void appendingNodeInEmptyDequeIsAlsoFirstNode(){
+            int item = 1;
+            deque.append(item);
+            assertEquals (item, deque.first());
+        }
+
+        @Test
+        void appendingANodeInEmptyDequeMakesItHaveSize1(){
+            int item = 1;
+            deque.append(item);
+            assertEquals(deque.size(), 1);
+        }
+
+
+
+        @Test
+        void appendingNodeInDequeIsLast(){
+            int item1 = 1;
+            int item2 = 2;
+            deque.append(item1);
+            deque.append(item2);
+            assertEquals(item2, deque.last());
+        }
+
+
+
+        @Test
+        void appendingANodeIncreasesSizeBy1(){
+            int item1 = 1;
+            int item2 = 2;
+            deque.append(item1);
+
+            int prevSize = deque.size();
+
+            deque.append(item2);
+            assertEquals(prevSize + 1, deque.size());
+        }
+    }
+
+    @Nested
+    @DisplayName("Casos de prueba de eliminaciones")
+    class Deletions{
+        @Test
+        void deletingFirstNodeFromEmptyDequeThrowsDoubleEndedQueueException(){
+            assertThrows(DoubleEndedQueueException.class, () -> deque.deleteFirst());
+        }
+
+        @Test
+        void deletingLastNodeFromEmptyDequeThrowsDoubleEndedQueueException(){
+            assertThrows(DoubleEndedQueueException.class, () -> deque.deleteLast());
+        }
+
+        @Test
+        void deletingFirstNodeDecreaseSizeBy1(){
+            int item1 = 1;
+            int item2 = 2;
+            deque.append(item1);
+
+            int prevSize = deque.size();
+
+            deque.deleteFirst();
+            assertEquals(prevSize - 1, deque.size());
+        }
+
+        @Test
+        void deletingLastNodeFromDequeDecreaseSizeBy1(){
+            int item1 = 1;
+            int item2 = 2;
+            deque.append(item1);
+
+            int prevSize = deque.size();
+
+            deque.deleteLast();
+            assertEquals(prevSize - 1, deque.size());
+        }
+    }
+
+    @Nested
+    @DisplayName("Propiedades")
+    class Properties{
+        @Test
+        void size1DequeFirstEqualsLast(){
+            int item = 1;
+            deque.prepend(item);
+            assertEquals (deque.first(), deque.last());
+        }
     }
 
 
-    @BeforeEach
-    void createDeque(){
-        deque = new DoublyLinkedListDeque<>();
-    }
 
 
 
-    @Test
-    void newDequeLastIsNull(){
-        assertNull(deque.last());
-    }
 
-    @Test
-    void newDequeSizeIs0(){
-        assertEquals(0, deque.size());
-    }
 
-    @Test
-    void prependingNodeInEmptyDequeIsTheFirstNode(){
-        int item = 1;
-        deque.prepend(item);
-        assertEquals(item, deque.first());
-    }
-
-    @Test
-    void prependingNodeInEmptyDequeIsAlsoTheLastNode(){
-        int item = 1;
-        deque.prepend(item);
-        assertEquals(item, deque.last());
-    }
-
-    @Test
-    void appendingNodeInEmptyDequeIsLastNode(){
-        int item = 1;
-        deque.append(1);
-        assertEquals (item, deque.last());
-    }
-
-    @Test
-    void appendingNodeInEmptyDequeIsAlsoFirstNode(){
-        int item = 1;
-        deque.append(item);
-        assertEquals (item, deque.first());
-    }
-
-    @Test
-    void size1DequeFirstEqualsLast(){
-        int item = 1;
-        deque.prepend(item);
-        assertEquals (deque.first(), deque.last());
-    }
-
-    @Test
-    void prependingANodeInEmptyDequeMakesItHaveSize1(){
-        int item = 1;
-        deque.prepend(item);
-        assertEquals(deque.size(), 1);
-    }
-
-    @Test
-    void appendingANodeInEmptyDequeMakesItHaveSize1(){
-        int item = 1;
-        deque.append(item);
-        assertEquals(deque.size(), 1);
-    }
-
-    @Test
-    void prependingNodeInDequeIsFirst() {
-        int item1 = 1;
-        int item2 = 2;
-        deque.prepend(item1);
-        deque.prepend(item2);
-        assertEquals(item2, deque.first());
-    }
-
-    @Test
-    void appendingNodeInDequeIsLast(){
-        int item1 = 1;
-        int item2 = 2;
-        deque.append(item1);
-        deque.append(item2);
-        assertEquals(item2, deque.last());
-    }
-
-    @Test
-    void prependingANodeIncreasesSizeBy1(){
-        int item1 = 1;
-        int item2 = 2;
-        deque.prepend(item1);
-
-        int prevSize = deque.size();
-
-        deque.prepend(item2);
-        assertEquals(prevSize + 1, deque.size());
-    }
-
-    @Test
-    void appendingANodeIncreasesSizeBy1(){
-        int item1 = 1;
-        int item2 = 2;
-        deque.append(item1);
-
-        int prevSize = deque.size();
-
-        deque.append(item2);
-        assertEquals(prevSize + 1, deque.size());
-    }
-
-    @Test
-    void deletingFirstNodeFromEmptyDequeThrowsDoubleEndedQueueException(){
-        assertThrows(DoubleEndedQueueException.class, () -> deque.deleteFirst());
-    }
-
-    @Test
-    void deletingLastNodeFromEmptyDequeThrowsDoubleEndedQueueException(){
-        assertThrows(DoubleEndedQueueException.class, () -> deque.deleteLast());
-    }
-
-    @Test
-    void deletingFirstNodeDecreaseSizeBy1(){
-        int item1 = 1;
-        int item2 = 2;
-        deque.append(item1);
-
-        int prevSize = deque.size();
-
-        deque.deleteFirst();
-        assertEquals(prevSize - 1, deque.size());
-    }
-
-    @Test
-    void deletingLastNodeFromDequeDecreaseSizeBy1(){
-        int item1 = 1;
-        int item2 = 2;
-        deque.append(item1);
-
-        int prevSize = deque.size();
-
-        deque.deleteLast();
-        assertEquals(prevSize - 1, deque.size());
-    }
 
 }
