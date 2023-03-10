72ebcb435cb0f43811b1479631bbc33fd2c99fcd
ignamarga1 <0619967319@uma.es>
2023-03-10 08:57:27 +0100

    Comentarios añadidos

--- src/test/java/org/mps/deque/DequeNodeTest.java
+++ src/test/java/org/mps/deque/DequeNodeTest.java
@@ -10,6 +10,32 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author Ignacio Martínez Gallardo
  */
 
+/* Casos de prueba
+*   1. Comprueba que el valor del nodo es 1
+*   2. Comprueba que al cambiar el valor del nodo a 6 este se actualiza correctamente
+*   3. Comprueba que al pedir el nodo anterior devuelve null
+*   4. Comprueba que al pedir el nodo posterior devuelve node
+*   5. Comprueba si es el primero, y como lo es se devuelve true
+*   6. Comprueba si es el último, y como no lo es se devuelve false
+*   7. Comprueba si no es un nodo terminal, y como no lo es se devuelve false
+*
+*   8. Comprueba que el valor del nodo es 5
+*   9. Comprueba que al cambiar el valor del nodo a 10 este se actualiza correctamente
+*   10. Comprueba que al pedir el nodo anterior devuelve previousNode
+*   11. Comprueba que al pedir el nodo posterior devuelve nextNode
+*   12. Comprueba si es el primero, y como no lo es se devuelve false
+*   13. Comprueba si es el último, y como no lo es se devuelve false
+*   14. Comprueba si no es un nodo terminal, y como lo es se devuelve true
+*
+*   15. Comprueba que el valor del nodo es 9
+*   16. Comprueba que al cambiar el valor del nodo a 1 este se actualiza correctamente
+*   17. Comprueba que al pedir el nodo anterior devuelve node
+*   18. Comprueba que al pedir el nodo posterior devuelve null
+*   19. Comprueba si es el primero, y como no lo es se devuelve false
+*   20. Comprueba si es el último, y como lo es se devuelve true
+*   21. Comprueba si no es un nodo terminal, y como no lo es se devuelve false
+* */
+
 @DisplayName("Un nodo")
 class DequeNodeTest {
     DequeNode<Integer> node, previousNode, nextNode;
@@ -83,7 +109,7 @@ class DequeNodeTest {
         }
 
         @Test
-        @DisplayName("si comprobamos que es uno intermedio devuelve false")
+        @DisplayName("si comprobamos que no es uno terminal devuelve false")
         void shouldReturnFalseToPreviousNodeNotBeingATerminalNode() {
             assertFalse(previousNode.isNotATerminalNode());
         }
